563b5f6f8e4718f002dba0d4b0329842263696a8
Иван Семов <ivan.semov.44@gmail.com>
2022-08-19 16:18:51 +0300

    update certificates

--- semov-portfolio/src/Components/Certificate/Certificate.js
+++ semov-portfolio/src/Components/Certificate/Certificate.js
@@ -1,6 +1,50 @@
-const Certificate =()=>{
-    return(
-        <p>certificate</p>
+import Carousel from "react-bootstrap/Carousel";
+import "./Certificate.css"
+
+const Certificate = () => {
+    return (
+        <div className="cert-div">
+            <Carousel variant="dark" interval={null}>
+                <Carousel.Item>
+                    <img 
+                        className="d-block w-100 cert-img"
+                        src="images/CSharpFundamentalsJanuary2020.jpeg"
+                        alt="C# Fundamentals"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 cert-img"
+                        src="images/JSAdvancedSeptember2021.jpeg"
+                        alt="JS Advanced"
+                    />
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 cert-img"
+                        src="images/JSApplicationsFebruary2022.jpeg"
+                        alt="Js Applications"
+                    />
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 cert-img"
+                        src="images/HTMLCSSMay2022.jpeg"
+                        alt="HTML and CSS"
+                    />
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 cert-img"
+                        src="images/ReactJSJune2022.jpeg"
+                        alt="React JS"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        </div>
     )
 }
 
--- semov-portfolio/src/Components/Projects/Projects.js
+++ semov-portfolio/src/Components/Projects/Projects.js
@@ -1,6 +1,39 @@
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
 const Projects = () => {
     return (
-        <p>Project</p>
+        <>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Simple Project from React js
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Simple Project from React js
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card><Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Simple Project from React js
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </>
     )
 }
 
